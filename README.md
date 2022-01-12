4de00defc13ca0f63d0cb9cd487543016747b836
tibrn <barontiberiu@gmail.com>
2022-01-12 14:51:29 +0200

    patch: change module name

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/configor"
+	"github.com/goldwindev/configor"
 )
 
 var Config = struct {
--- unmarshal_json_test.go
+++ unmarshal_json_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/jinzhu/configor"
+	"github.com/goldwindev/configor"
 )
 
 func TestUnmatchedKeyInJsonConfigFile(t *testing.T) {
